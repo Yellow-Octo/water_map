6f7cb106d2ca0d1415c329bd1ae0c40a86a24fba
Michael Qin <1mike12@gmail.com>
2024-07-25 13:06:57 -0400

    working copy import

--- docker-entrypoint-initdb.d/2_import_shapefiles.sh
+++ docker-entrypoint-initdb.d/2_import_shapefiles.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
 
-# Import shapefiles into PostGIS
-# -a : Append to the existing table
-# -s : SRID of the shapefile
-shp2pgsql -a -s 4326 /data/$SHAPE_FILE_NAME $TABLE_NAME | awk -F"'" '/INSERT INTO/ {print "INSERT INTO '"$TABLE_NAME"' (geom) VALUES ('\''" $(NF-1) "'\'');"}' | psql -U $POSTGRES_USER -d $POSTGRES_DB
+# Create a temporary file
+TEMP_FILE=$(mktemp)
+
+# Import shapefiles into PostGIS and prepare data for COPY
+shp2pgsql -a -s 4326 /data/$SHAPE_FILE_NAME $TABLE_NAME |
+awk -F"'" '/INSERT INTO/ {print $(NF-1)}' > $TEMP_FILE
+
+# Use COPY to bulk insert data
 psql -U $POSTGRES_USER -d $POSTGRES_DB <<-EOSQL
+COPY $TABLE_NAME (geom) FROM '$TEMP_FILE' WITH (FORMAT text);
 UPDATE $TABLE_NAME SET $WATER_TYPE_COLUMN_NAME = 1 WHERE $WATER_TYPE_COLUMN_NAME IS NULL;
 EOSQL
+
+# Remove temporary file
+rm $TEMP_FILE
